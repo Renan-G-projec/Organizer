5e82cbe17889fbcdfda75b28506f2c4dfb29b578
Renan-G-projec <renanfartes@gmail.com>
2025-12-29 13:29:34 -0300

    Implementing the configuration wizard

---
+++ config.py
@@ -0,0 +1,18 @@
+import json
+config_file = open("config.json", "w")
+
+# Initial menu
+print("="*41)
+print("PYTHON NOTE ORGANIZER - Configurations".center(41, " "))
+print("="*41)
+
+# Configuration
+path = input("Insert the absolute path that the files should be moved: ")
+sufix = input("Insert the sufix that the files may have after the number of the day: ")
+
+# Parsing and writing JSON
+config_dict = {"path": path, "sufix": sufix}
+config_file.write(json.dumps(config_dict, indent=4))
+
+# Finalization
+print("=" * 41)
